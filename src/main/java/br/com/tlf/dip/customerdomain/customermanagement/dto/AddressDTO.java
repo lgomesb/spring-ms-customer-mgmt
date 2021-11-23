1a44d46143e78cbd4d06e0c9b15b868f2caed5d3
leandro <lgomesb@gmail.com>
2021-11-23 00:38:17 -0300

    [Test] Implementando teste class CustomerManagementController metodos upda e delete

--- src/main/java/br/com/tlf/dip/customerdomain/customermanagement/dto/AddressDTO.java
+++ src/main/java/br/com/tlf/dip/customerdomain/customermanagement/dto/AddressDTO.java
@@ -1,10 +1,5 @@
 package br.com.tlf.dip.customerdomain.customermanagement.dto;
 
-import java.time.LocalDateTime;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import br.com.tlf.dip.customerdomain.customermanagement.model.Address;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,27 +17,5 @@ public class AddressDTO {
 	private String neighborhood;
 	private String street;
 	private String streetNumber;
-	
-	public AddressDTO(Address a) {
-		this.city = a.getCity();
-		this.country = a.getCountry();
-		this.neighborhood = a.getNeighborhood();
-		this.state = a.getState();
-		this.street = a.getStreet();
-		this.streetNumber = a.getStreetNumber();
-	}
 
-	@JsonIgnore
-	public Address getAddress() {
-					
-		return Address.builder()
-				.city(this.city)
-				.country(this.country)
-				.state(this.state)
-				.neighborhood(this.neighborhood)
-				.street(this.street)
-				.streetNumber(this.streetNumber)
-				.lastDate(LocalDateTime.now())
-				.build();
-	}
 }
--- src/main/java/br/com/tlf/dip/customerdomain/customermanagement/dto/ContactMediumDTO.java
+++ src/main/java/br/com/tlf/dip/customerdomain/customermanagement/dto/ContactMediumDTO.java
@@ -1,10 +1,5 @@
 package br.com.tlf.dip.customerdomain.customermanagement.dto;
 
-import java.time.LocalDateTime;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import br.com.tlf.dip.customerdomain.customermanagement.model.ContactMedium;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,22 +14,5 @@ public class ContactMediumDTO {
 	private String emailAddress;
 	private String phoneNumber;
 	private Boolean preferred;
-	
-	public ContactMediumDTO(ContactMedium c) {
-		
-		this.emailAddress = c.getEmailAddress();
-		this.phoneNumber = c.getPhoneNumber();
-		this.preferred = c.getPreferred();		
-	}
 
-	@JsonIgnore
-	public ContactMedium getContact() {
-		
-		return ContactMedium.builder()
-				.emailAddress(this.emailAddress)
-				.phoneNumber(this.phoneNumber)
-				.preferred(this.preferred)
-				.lastUpdate(LocalDateTime.now())
-				.build();
-	}
 }
